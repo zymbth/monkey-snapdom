8dcafac2828422c1fe307c32739d648a1e61ad8b
ymzhao <ymzhaobth@gmail.com>
2025-09-08 18:44:41 +0800

    工具类封装测试

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,26 @@
 <script setup>
 import { getCurrentInstance, ref } from 'vue'
-import { manualDelay, shining } from './utils.js'
+import SnapdomUtil from './utils/SnapdomUtil'
 
-let hoverEl = null
 const isChosing = ref(false)
 const loading = ref(false)
 
-let currSnapdom = window.snapdom
-if(process.env.NODE_ENV === 'development') {
-  try {
-    import('@zumer/snapdom').then(module => {
-      currSnapdom = module.snapdom
-    })
-  } catch {}
-}
-
-// 开启功能
-function startChosing() {
-  if(isChosing.value || loading.value) {
-    console.warn('正在选中，已重新开始')
-    stopChosing()
-  }
-  isChosing.value = true
-  document.body.addEventListener('mousemove', handleMousemove)
-  setTimeout(() => {
-    document.body.addEventListener('click', handleConfirmTarget)
-    document.body.addEventListener('keydown', handleEsc)
-  }, 200)
-}
-
-// 关闭功能
-function stopChosing() {
-  isChosing.value = false
-  document.body.removeEventListener('mousemove', handleMousemove)
-  document.body.removeEventListener('click', handleConfirmTarget)
-  document.body.removeEventListener('keydown', handleEsc)
-  if(hoverEl) {
-    hoverEl.classList.remove('snap-target')
-    hoverEl = null
-  }
-}
-
-// 鼠标移动
-function handleMousemove(e) {
-  if(!isChosing.value || !e.target) return
-  if(hoverEl) hoverEl.classList.remove('snap-target')
-  e.target.classList.add('snap-target')
-  hoverEl = e.target
-}
-
-// 点击确认选中
-// @ts-ignore
-async function handleConfirmTarget(e) {
-  if(!hoverEl) {
-    shining('未选中目标', 'orange')
-    return
-  }
-  const tmpEl = hoverEl
-  stopChosing()
-  // 下载
-  loading.value = true
-  await manualDelay(50)
-  try {
-    await execSnapDom(tmpEl)
-    shining('下载成功')
-  } catch(err) {
-    shining('下载失败', 'red')
-    console.error(err)
-  }
-  loading.value = false
-}
-
-// ESC取消选中
-function handleEsc(e) {
-  if(e.keyCode !== 27) return
-  stopChosing()
-}
-
-// 指定DOM元素，下载其内容
-async function execSnapDom(targetEl) {
-  // @ts-ignore
-  if(!currSnapdom) {
-    console.error('未加载插件snapdom')
-    return
-  }
-  if(!targetEl) {
-    console.error('未指定捕获元素')
-    return
-  }
-  console.time('捕获耗时：')
-  // @ts-ignore
-  const capture = await currSnapdom(targetEl, {
-    embedFonts: true,
-    compress: true
-  })
-  console.timeEnd('捕获耗时：')
-  console.time('下载耗时：')
-  await capture.download({
-    format: 'jpg',
-    filename: 'chart-report-2025'
-  });
-  console.timeEnd('下载耗时：')
-}
+const snapdomUtil = new SnapdomUtil(isChosing, loading)
+console.log('snapdomUtil: ', snapdomUtil)
 
 // 暴露给全局
 const instance = getCurrentInstance()
-instance.appContext.config.globalProperties.$startChosing = startChosing
-instance.appContext.config.globalProperties.$stopChosing = stopChosing
+instance.appContext.config.globalProperties.$startChosing = () => snapdomUtil.startChosing()
+instance.appContext.config.globalProperties.$stopChosing = () => snapdomUtil.stopChosing()
 
+
+function onCustomSnap() {
+  snapdomUtil.stopChosing()
+}
 </script>
 
 <template>
-  <div id="snap-app">截</div>
+  <div id="snap-app" :class="{ hide: isChosing, loading }" @click="onCustomSnap">截</div>
 </template>
 
 <style scoped>
@@ -125,5 +34,12 @@ instance.appContext.config.globalProperties.$stopChosing = stopChosing
   color: #fff;
   padding: 10px;
   border-radius: 5px;
+}
+.hide {
+  display: none;
+}
+.loading {
+  opacity: 0.5;
+  cursor: progress;
 }
 </style>
---
+++ src/utils/SnapdomUtil.js
@@ -0,0 +1,116 @@
+import { manualDelay, shining } from '../utils.js'
+
+export default class SnapdomUtil {
+  // 构造函数
+  constructor(isChosing, loading) {
+    this._getSnapDom()
+    this._isChosing = isChosing
+    this._loading = loading
+    this._hoverEl = null
+  }
+  
+  async _getSnapDom() {
+    let tmp = window?.snapdom
+    if(!tmp && process.env.NODE_ENV === 'development') {
+      try {
+        tmp = await import('@zumer/snapdom').then(module => module.snapdom)
+      } catch {}
+    }
+    this._snapdom = tmp
+  }
+
+  // 开启功能
+  startChosing() {
+    if(this._isChosing.value || this._loading.value) {
+      console.warn('正在选中，已重新开始')
+      this.stopChosing()
+    }
+    this._isChosing.value = true
+    document.body.addEventListener('mousemove', this._handleMousemove)
+    setTimeout(() => {
+      document.body.addEventListener('click', this._handleConfirmTarget)
+      document.body.addEventListener('keydown', this._handleEsc)
+    }, 200)
+  }
+
+  // 关闭功能
+  stopChosing() {
+    this._isChosing.value = false
+    document.body.removeEventListener('mousemove', this._handleMousemove)
+    document.body.removeEventListener('click', this._handleConfirmTarget)
+    document.body.removeEventListener('keydown', this._handleEsc)
+    if(this._hoverEl) {
+      this._hoverEl.classList.remove('snap-target')
+      this._hoverEl = null
+    }
+  }
+
+  destroy() {
+    this._loading = null
+    this._isChosing = null
+    this.startChosing = () => {}
+    this.stopChosing()
+  }
+
+  // 鼠标移动
+  _handleMousemove = (e) => {
+    if(!this._isChosing.value || !e.target) return
+    if(this._hoverEl) this._hoverEl.classList.remove('snap-target')
+    e.target.classList.add('snap-target')
+    this._hoverEl = e.target
+  }
+
+  // 点击确认选中
+  // @ts-ignore
+  _handleConfirmTarget = async (e) => {
+    if(!this._hoverEl) {
+      shining('未选中目标', 'orange')
+      return
+    }
+    const tmpEl = this._hoverEl
+    this.stopChosing()
+    // 下载
+    this._loading.value = true
+    await manualDelay(50)
+    try {
+      await this._execSnapDom(tmpEl)
+      shining('下载成功')
+    } catch(err) {
+      shining('下载失败', 'red')
+      console.error(err)
+    }
+    this._loading.value = false
+  }
+
+  // ESC取消选中
+  _handleEsc = (e) => {
+    if(e.keyCode !== 27) return
+    this.stopChosing()
+  }
+
+  // 指定DOM元素，下载其内容
+  async _execSnapDom (targetEl) {
+    // @ts-ignore
+    if(!this._snapdom) {
+      console.error('未加载插件snapdom')
+      return
+    }
+    if(!targetEl) {
+      console.error('未指定捕获元素')
+      return
+    }
+    console.time('捕获耗时：')
+    // @ts-ignore
+    const capture = await this._snapdom(targetEl, {
+      embedFonts: true,
+      compress: true
+    })
+    console.timeEnd('捕获耗时：')
+    console.time('下载耗时：')
+    await capture.download({
+      format: 'jpg',
+      filename: 'chart-report-2025'
+    });
+    console.timeEnd('下载耗时：')
+  }
+}
